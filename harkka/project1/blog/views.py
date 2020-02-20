48ebb4bd5ea70cc05903312b87c18f340dda9175
unknown <matias.eko@tuni.fi>
2020-02-20 14:19:20 +0200

    uusi

--- harkka/project1/blog/views.py
+++ harkka/project1/blog/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return HttpResponse('<h1>Blog Home</h1>')
+    return HttpResponse('<h1>Blog Hhhhhome</h1>')
 
 
 def about(request):
